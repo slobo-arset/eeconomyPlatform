6be974dd23153db8ed2ac46a92e92b83fde07173
slobo-arset <slobostjepanovic@gmail.com>
2024-08-18 18:03:56 +0200

    add hidration state service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MainStateService } from './data-access/state/main-state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cpointClient';
+export class AppComponent implements OnInit {
+  title = 'Stručne usluge - e-EconomY';
+
+  @HostListener('window:beforeunload') onRefresh() {
+    if(this.mainStateService.getState() && Object.keys(this.mainStateService.getState()).length >0) {
+        sessionStorage.setItem('state', JSON.stringify( this.mainStateService.getState()))
+    }
+  }
+
+  constructor(
+    public mainStateService: MainStateService
+  ){}
+
+  ngOnInit() {
+
+    const state = sessionStorage.getItem('state');
+
+    if(state) {
+        this.mainStateService.setAppState(JSON.parse(state));
+        sessionStorage.removeItem('state');
+    }
+
+  }
+
 }
