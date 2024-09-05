d43e5e2ff1a612b5c887474ac46729d578d67022
slobo-arset <slobostjepanovic@gmail.com>
2024-09-05 08:29:30 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://api.tiketi.rs/api/'
-  apiUrl: 'http://localhost:5002/api/'
+  apiUrl: 'https://api.tiketi.rs/api/'
+  //apiUrl: 'http://localhost:5002/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://api.e-economy.biz/api/'
-  apiUrl: 'http://localhost:5002/api/'
+  apiUrl: 'https://api.e-economy.biz/api/'
+  //apiUrl: 'http://localhost:5002/api/'
 };
